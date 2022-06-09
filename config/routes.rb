7f71af3fbd4292b325b63c46adeeb5105870c1f7
izumi-hiro <transformers2728frost@gmail.com>
2022-06-09 10:34:10 +0000

    [Add] routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,39 @@
 Rails.application.routes.draw do
+  
   # 顧客用
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
   
-  resources :customers do
+  scope module: :public do
+    # homes
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
+    
+    # customers
+    get "customers/unsubscribe" => "customers#unsubscribe"
+    patch "customers/withdraw" => "customers#withdraw"
+    resources :customers, only: [:show, :edit, :update] do
+      
+      # relationships
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+    
+    # items
+    resources :items do
+      
+      # favorites
+      resource :favorites, only: [:create, :destroy]
+      
+      #item_comments
+      resources :item_comments, only: [:create, :destroy]
+    end
+    
+    # searches
+    get "search" => "searches#search"
     
   end
 
@@ -13,5 +41,18 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    # customers
+    resources :customers, only: [:index, :show, :edit, :update]
+    
+    # items
+    resources :items, only: [:index, :show, :edit, :update]
+    
+    # searches
+    get "search" => "searches#search"
+    
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
