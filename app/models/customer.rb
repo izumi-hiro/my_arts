f8a91dc28a11b8bd7e4b4170e7d3496dcbc969a0
izumi-hiro <transformers2728frost@gmail.com>
2022-06-11 11:01:29 +0000

    [Add]フォロー機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,19 +3,19 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :items, dependent: :destroy
   has_many :item_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -23,5 +23,17 @@ class Customer < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
+  def follow(customer)
+    relationships.create(followed_id: customer.id)
+  end
+
+  def unfollow(customer)
+    relationships.find_by(followed_id: customer.id).destroy
+  end
+
+  def following?(customer)
+    followings.include?(customer)
+  end
+
 end
