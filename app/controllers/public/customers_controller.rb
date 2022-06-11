3e52b289588d480d559fbce7ef6f0e19df27d017
izumi-hiro <transformers2728frost@gmail.com>
2022-06-11 12:12:50 +0000

    [Add]会員情報編集画面、エラーメッセージ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,15 +5,28 @@ class Public::CustomersController < ApplicationController
   end
   
   def edit
+    @customer = Customer.find(params[:id])
   end
   
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "会員情報を変更しました"
+    else
+      render "edit"
+    end
   end
   
   def unsubscribe
   end
   
   def withdraw
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :profile_image)
   end
   
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
   has_many :items, dependent: :destroy
   has_many :item_comments, dependent: :destroy
