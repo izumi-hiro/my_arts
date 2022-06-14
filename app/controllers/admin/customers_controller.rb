bb5000ba613f6c86c48580b4cf77932ff8c93a31
izumi-hiro <transformers2728frost@gmail.com>
2022-06-14 04:35:19 +0000

    [Add] admin/customers/index show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,31 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
+    @customers = Customer.all
   end
-  
+
   def show
+    @customer = Customer.find(params[:id])
+    @items = @customer.items.includes(:item_images).order("created_at DESC")
   end
-  
+
   def edit
+    @customer = Customer.find(params[:id])
   end
-  
+
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path, notice: "更新に成功しました"
+    else
+      render "edit"
+    end
   end
-  
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :profile_image, :is_deleted)
+  end
+
 end
