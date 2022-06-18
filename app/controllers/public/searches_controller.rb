b794f3a47ab3c384c5eac2909b06a4f81f7a739b
izumi-hiro <transformers2728frost@gmail.com>
2022-06-18 07:40:16 +0000

    [Add] 検索機能

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,28 @@
 class Public::SearchesController < ApplicationController
   
-  def searche
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+  
+  private
+  
+  def search_for(model, content, method)
+    if model == 'customer'
+      if method == 'perfect'
+        Customer.where(name: content)
+      else
+        Customer.where('name LIKE ?', '%'+content+'%')
+      end
+    elsif model == 'item'
+      if method == 'perfect'
+        Item.where(title: content)
+      else
+        Item.where('title LIKE ?', '%'+content+'%')
+      end
+    end
   end
   
 end
