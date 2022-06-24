245ea460a1bb8defd67bd6a3ccc3e8328bfa556e
izumi-hiro <transformers2728frost@gmail.com>
2022-06-24 11:47:34 +0000

    [Update] 作者の検索結果から、会員ステータスが退会の作者を除外

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -12,15 +12,17 @@ class Public::SearchesController < ApplicationController
   def search_for(model, content, method)
     if model == 'customer'
       if method == 'perfect'
-        Customer.where(name: content)
+        # 会員ステータスがfalse(有効)の会員に絞る
+        Customer.where(name: content, is_deleted: false)
       else
-        Customer.where('name LIKE ?', '%'+content+'%')
+        Customer.where('name LIKE ?', '%'+content+'%').where(is_deleted: false)
       end
     elsif model == 'item'
       if method == 'perfect'
-        Item.where(title: content)
+        # 作品ステータスがtrue(公開)かつ作者の会員ステータスがfalse(有効)の作品に絞る。includesで他テーブルの情報を取得
+        Item.where(title: content, is_active: true, customer: { is_deleted: false}).includes(:customer, :item_images)
       else
-        Item.where('title LIKE ?', '%'+content+'%')
+        Item.where('title LIKE ?', '%'+content+'%').where(is_active: true, customer: { is_deleted: false}).includes(:customer, :item_images)
       end
     end
   end
