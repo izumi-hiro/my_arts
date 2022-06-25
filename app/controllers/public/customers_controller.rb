943547f5792306407cc4df52cd2ee814ef10760e
izumi-hiro <transformers2728frost@gmail.com>
2022-06-25 10:38:20 +0000

    [Update] URL入力で退会済みの会員の詳細ページにアクセスできる問題を解消

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,13 +3,17 @@ class Public::CustomersController < ApplicationController
   before_action :ensure_correct_customer, only: [:edit, :update, :withdraw]
 
   def show
-    @customer = Customer.find(params[:id])
-    @items = @customer.items.includes(:item_images).order("created_at DESC")
-    favorites = Favorite.where(customer_id: @customer.id).pluck(:item_id)
-    #会員ステータスがtrue（退会）の作者をdeleted_customer_idsに代入。
-    deleted_customer_ids = Customer.where(is_deleted: true)
-    #お気に入り一覧の作品は、いいねをした作品かつ作品の公開ステータスがtrueである。ただし、deleted_customer_idsに該当する作品は除外。
-    @favorite_list = Item.where(id: favorites, is_active: true).where.not(customer_id: deleted_customer_ids)
+      @customer = Customer.find(params[:id])
+      if @customer.is_deleted == '_valid'
+        @items = @customer.items.includes(:item_images).order("created_at DESC")
+        favorites = Favorite.where(customer_id: @customer.id).pluck(:item_id)
+        #会員ステータスがtrue（退会）の作者をdeleted_customer_idsに代入。
+        deleted_customer_ids = Customer.where(is_deleted: true)
+        #お気に入り一覧の作品は、いいねをした作品かつ作品の公開ステータスがtrueである。ただし、deleted_customer_idsに該当する作品は除外。
+        @favorite_list = Item.where(id: favorites, is_active: true).where.not(customer_id: deleted_customer_ids)
+      else
+        redirect_to items_path
+      end
   end
 
   def edit
