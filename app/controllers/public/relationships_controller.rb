1b1667997a2a93b26bc89376e4d614705ab8c5f5
izumi-hiro <transformers2728frost@gmail.com>
2022-06-26 17:11:19 +0000

    [Add] フォローボタンのlinktoにremote trueを記述。エラーを解消したが、リロードしないとボタンが変化しない

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,14 +3,13 @@ class Public::RelationshipsController < ApplicationController
   def create
     customer = Customer.find(params[:customer_id])
     current_customer.follow(customer)
-		redirect_to request.referer
-		# redirect_toを削除するとエラーになるActionController::UnknownFormat in Public::RelationshipsController#create  Public::RelationshipsController#create is missing a template for this request format and variant.
+		# redirect_to request.referer
   end
 
   def destroy
     customer = Customer.find(params[:customer_id])
     current_customer.unfollow(customer)
-		redirect_to request.referer
+		# redirect_to request.referer
   end
 
   def followings
